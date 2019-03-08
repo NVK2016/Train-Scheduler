10d4b5751893411c40c76824c16b2c52a77e7d8d
Namita Shenai <namita.shenai@gmail.com>
2019-03-07 19:06:42 -0800

    Added database reference & updated the HTML file

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAcfDGkuiuuRPaAiQ_3Nf9weBUDu94id9c",
+    authDomain: "train-schedules-e0160.firebaseapp.com",
+    databaseURL: "https://train-schedules-e0160.firebaseio.com",
+    projectId: "train-schedules-e0160",
+    storageBucket: "train-schedules-e0160.appspot.com",
+    messagingSenderId: "500974018540"
+  };
+
+  firebase.initializeApp(config);
+
+  
