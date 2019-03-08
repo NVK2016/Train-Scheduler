4f25a3784335379fe0ce6d61dc64811173975f17
Namita Shenai <namita.shenai@gmail.com>
2019-03-07 23:40:21 -0800

    Pseudo coded the logic for showing the next train

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -12,13 +12,17 @@ $(document).ready(function () {
 
     firebase.initializeApp(config);
 
+    // GLOBAL VARIABLES 
+    //---------------------------------------------------- 
     var database = firebase.database(); 
+    
+    // .format("hh:mm A") here hh- stands for 1-12 hr, mm for minutues , A for am/pm  [http://momentjs.com/docs/#/parsing/string-format/] 
+    var currentTime = moment().format("hh:mm A"); //Military time format 
+    $("#current-time").append(currentTime); 
 
-    //FUNCTION 
-    //------------------------------------
-    function displayTrainSchedule() {
-
-    }
+    var trainName , trainDest ;
+    var firstTrainTime = 00;
+    var trainFrequency = 0;
 
     //EVENT LISTINERS 
     //----------------------------
@@ -29,10 +33,10 @@ $(document).ready(function () {
         event.preventDefault();
 
         //Grabs value from the fields 
-        var trainName = $("#train-name-input").val().trim();
-        var trainDest = $("#destination-input").val().trim();
-        var firstTrainTime = $("#first-train-input").val().trim();
-        var trainFreq = $("#frequency-input").val().trim();
+        trainName = $("#train-name-input").val().trim();
+        trainDest = $("#destination-input").val().trim();
+        firstTrainTime = $("#first-train-input").val().trim();
+        trainFrequency = $("#frequency-input").val().trim();
 
         // Code for the push data into the Firebase 
         database.ref().push({
@@ -40,7 +44,7 @@ $(document).ready(function () {
             trainName: trainName,
             destination: trainDest,
             firstTrain: firstTrainTime,
-            frequency: trainFreq,
+            frequency: trainFrequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
 
@@ -55,17 +59,31 @@ $(document).ready(function () {
         trainName = childSnapshot.val().trainName; 
         trainDest = childSnapshot.val().destination; 
         firstTrainTime = childSnapshot.val().firstTrain; 
-        trainFreq = childSnapshot.val().frequency; 
+        trainFrequency = childSnapshot.val().frequency; 
 
+        var nextArrival , minutesAway; 
+        
+        // First trains is 06:10 am , frequency is set every 20 min 
+        //current time is 06:45 am , 
+        //Difference between the first train time & current time 
+        //Time difference = 06:45 am - 06:10 am 
+        //time difference ? by frequency 
+        // To calculate the next arrival time 
+        
+        
         //Append data to rows 
         var newRow = $("<tr>");
         newRow.append("<td>" + trainName + "</td>");
         newRow.append("<td>" + trainDest + "</td>");
-        newRow.append("<td>" + trainFreq + "</td>");
-        newRow.append("<td>" +  + "</td>"); //blank data 
-        newRow.append("<td>" + + "</td>"); //blank data 
+        newRow.append("<td>" + trainFrequency + "</td>");
+        newRow.append("<td>" + nextArrival  + "</td>"); //blank data 
+        newRow.append("<td>" + minutesAway+ "</td>"); //blank data 
 
         // Append row to table
         $("#train-schedule-body").append(newRow);
+
+        // Handles any database errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
 }); 
