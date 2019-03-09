27e62eab00fe5f22683e21a82a99e7d1dbd1245d
Namita Shenai <namita.shenai@gmail.com>
2019-03-08 18:04:00 -0800

    updated the table header

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -18,8 +18,9 @@ $(document).ready(function () {
     
     // .format("hh:mm A") here hh- stands for 1-12 hr, mm for minutues , A for am/pm  [http://momentjs.com/docs/#/parsing/string-format/] 
     var currentTime = moment().format("hh:mm A"); //Military time format 
-    $("#current-time").append(currentTime); 
-    setInterval($("#current-time").append(currentTime), 60000); 
+
+    var interTime = setInterval($("#current-time").append(currentTime), 1000); 
+  clearInterval(interTime);
 
     var trainName , trainDest ;
     var firstTrainTime = 00;
@@ -70,25 +71,18 @@ $(document).ready(function () {
 
         var nextTrainArrival , minutesAway; 
         
-        // First trains is 06:10 am , frequency is set every 20 min 
-        //current time is 06:45 am , 
-        //Difference between the first train time & current time 
-        //Time difference = 06:45 am - 06:10 am 
-        //time difference ? by frequency 
-        // To calculate the next arrival time 
-        
         // Convert time 
         // First Time (pushed back 1 year to make sure it comes before current time)
         var timeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-        console.log("timeConverted:" +timeConverted);
+        // console.log("timeConverted:" +timeConverted);
 
          //difference between first time and current time 
          var timeDifference = moment().diff(moment(timeConverted),"m");
-         console.log("timeDifference:" + timeDifference);
+        //  console.log("timeDifference:" + timeDifference);
 
          //minutes since last train left diffTime%frequency
         var timeApart = timeDifference % trainFrequency;
-        console.log("time apart:" +timeApart);
+        // console.log("time apart:" +timeApart);
 
         minutesAway = trainFrequency - timeApart;
         // To calculate the arrival time, add minutesAwat=y to the currrent time
@@ -109,6 +103,10 @@ $(document).ready(function () {
     }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
+
+      // refreshes the table 
+      var table = document.getElementById ("#train-table");
+      table.refresh ();
 
     //Display in descending order 
     // dataRef.ref().orderByChild("frequency").on("child_added", function(snapshot) {
